4beb11c300ab6e133f10a469ff42d64a1d9e089f
netomathias <ivonir.mathiasneto@gmail.com>
2022-08-03 21:01:20 -0300

    :wrench: chore: add bean in main class to modelmapper

--- src/main/kotlin/br/com/neto/catalogapi/CatalogApiApplication.kt
+++ src/main/kotlin/br/com/neto/catalogapi/CatalogApiApplication.kt
@@ -1,11 +1,20 @@
 package br.com.neto.catalogapi
 
+import org.modelmapper.ModelMapper
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class CatalogApiApplication
+class CatalogApiApplication {
+
+	@Bean
+	fun ModelMapper(): ModelMapper {
+		return org.modelmapper.ModelMapper()
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<CatalogApiApplication>(*args)
 }
+
